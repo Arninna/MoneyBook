bc102165e0098b43289d5c4362d8fae0185b0f02
//Andre <andrea.arnaboldi.2001@gmail.com>
2024-04-22 18:59:02 +0200

    Aggiunta Auth con firebase. Aggiunta registrazione (creazione utente) con email e password con Firebase

--- app/src/main/java/com/example/moneybook/LoginActivity.kt
+++ app/src/main/java/com/example/moneybook/LoginActivity.kt
@@ -1,11 +1,44 @@
 package com.example.moneybook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import com.example.moneybook.databinding.ActivityLoginBinding
+import com.example.moneybook.databinding.ActivityRegistrationBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    /*private lateinit var email:EditText
+    private lateinit var password:EditText
+    private lateinit var btnLogin:Button
+    private lateinit var forgotPwd :TextView
+    private lateinit var signupTextview:TextView*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            login()
+        }
+
+        binding.forgetPassword.setOnClickListener{
+
+        }
+
+        binding.signupReg.setOnClickListener{
+            val registerIntent = Intent(this,RegistrationActivity::class.java)
+            startActivity(registerIntent)
+        }
+    }
+
+    fun login(){
+        val email = binding.emailLogin.text.toString().trim()
+        val password = binding.passwordLogin.text.toString().trim()
+
     }
 }
--- app/src/main/java/com/example/moneybook/RegistrationActivity.kt
+++ app/src/main/java/com/example/moneybook/RegistrationActivity.kt
@@ -1,5 +1,6 @@
 package com.example.moneybook
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -7,10 +8,12 @@ import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import com.example.moneybook.databinding.ActivityRegistrationBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegistrationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityRegistrationBinding
+    private lateinit var firebaseAuth: FirebaseAuth
     /*private lateinit var  email: EditText
     private lateinit var  password: EditText
     private lateinit var  btnRegister: Button
@@ -22,14 +25,30 @@ class RegistrationActivity : AppCompatActivity() {
         binding = ActivityRegistrationBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        firebaseAuth = FirebaseAuth.getInstance()
+
         binding.btnRegister.setOnClickListener {
             registration()
+        }
+
+        binding.signinTextview.setOnClickListener{
+            val signinIntent = Intent(this,LoginActivity::class.java)
+            startActivity(signinIntent)
         }
     }
 
     fun registration(){
         val email = binding.emailRegister.text.toString().trim()
         val password = binding.passwordRegister.text.toString().trim()
-        Toast.makeText(this,email + " " + password,Toast.LENGTH_LONG).show()
+        if(email.isNotEmpty() && password.isNotEmpty()){
+            firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener {
+                if(it.isSuccessful){
+                    val intent = Intent(this,LoginActivity::class.java)
+                    startActivity(intent)
+                }else
+                    Toast.makeText(this,it.exception.toString(), Toast.LENGTH_SHORT).show()
+            }
+        }else
+            Toast.makeText(this,"Tutti i campi devono essere completi",Toast.LENGTH_LONG).show()
     }
 }
